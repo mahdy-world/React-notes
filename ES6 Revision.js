f44996ba647c6e85650ed02872eeccc12fdfe49e
mahdy-world <mmos7239@gmail.com>
2023-06-22 16:09:09 +0300

    arrow function

--- ES6 Revision.js
+++ ES6 Revision.js
@@ -10,3 +10,23 @@ function foo(){
     }
     console.log(i)
 ;}
+
+
+// Arrow function
+// Create arrow function
+const foo = () => {
+    console .log("This is my first arrow function");
+}
+// If we have one line in function we can write it like this:
+const foo = () => console.log("ahmed");
+// To refere to the object using arrow:
+const ahmed = {
+    name: "ahmed",
+    age: 20,
+    foo: function (){
+        // we add arow function in normal function to refare to the object
+        const inner = () => console.log(this);
+        inner();
+    }
+
+}
